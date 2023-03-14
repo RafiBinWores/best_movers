0984568c17f47aaba01883a9902f50e60000f19a
RafiBinWores <rafibinwores2203@gmail.com>
2023-03-15 00:31:02 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+const hamburger = document.querySelector('.hamburger');
+const mobile = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', function(){
+  hamburger.classList.toggle('is-active');
+  mobile.classList.toggle('is-active');
+});
+
+
+var fullImgBox = document.getElementById('fullImgBox');
+var fullImg = document.getElementById('fullImg');
+
+function openFullImg(photo) {
+    fullImgBox.style.display = "flex";
+    fullImg.src = photo;
+  };
+  
+  function closeFullImg() {
+    fullImgBox.style.display = "none";
+  };
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    centerSlide: 'true',
+    grabCursor: 'true',
+    fade: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
